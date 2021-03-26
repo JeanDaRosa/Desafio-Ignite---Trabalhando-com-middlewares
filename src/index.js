9e6277b87a009e7e593ce62390d4467b5b3e2433
JeanDaRosa <unrealsaifer@gmail.com>
2021-03-26 19:08:42 -0300

    Creating the midlewares

--- src/index.js
+++ src/index.js
@@ -10,19 +10,70 @@ app.use(cors());
 const users = [];
 
 function checksExistsUserAccount(request, response, next) {
-  // Complete aqui
+  const { username } = request.headers;
+
+  const user = users.find(user => user.username === username)
+
+  if (!user){
+    return response.status(404).json({error: "Username do not exist!"})
+  }
+
+  request.user = user;
+
+  return next();
 }
 
 function checksCreateTodosUserAvailability(request, response, next) {
-  // Complete aqui
+  const { user } = request;
+
+  if (user.pro === false && user.todos.length >= 10) {
+   
+    return response.status(403).json({error:""})
+    
+  }
+
+return next()
 }
 
 function checksTodoExists(request, response, next) {
-  // Complete aqui
+  const { username } = request.headers;
+  const { id } = request.params;
+
+
+  const user = users.find((user) => user.username === username)
+  
+
+  if (!user) {
+    return response.status(404).json({error: "Username alredy exist!"})
+  }
+
+  if (!validate(id)) {
+    return response.status(400).json({error: "Not valid ID!"})
+  }
+
+  const toDo = user.todos.find((todo) => todo.id === id)
+  if (!toDo) {
+    return response.status(404).json({error: "Todo not found"})
+  }
+  
+  request.user = user;  
+  request.todo = toDo;
+
+  return next()
 }
 
 function findUserById(request, response, next) {
-  // Complete aqui
+  const { id } = request.params;
+
+  const user = users.find(user => user.id === id);
+
+  if (!user) {
+    return response.status(404).json({error:"User not found!"})
+  };
+
+  request.user = user;
+
+  return next();
 }
 
 app.post('/users', (request, response) => {
@@ -48,9 +99,9 @@ app.post('/users', (request, response) => {
 });
 
 app.get('/users/:id', findUserById, (request, response) => {
-  const { user } = request;
+   const { user } = request;
 
-  return response.json(user);
+   return response.json(user);
 });
 
 app.patch('/users/:id/pro', findUserById, (request, response) => {
